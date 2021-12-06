721f17b422b1a40bffd2be8d79988727dc67235a
Marko Zagožen <marko.zagozen@gmail.com>
2021-12-06 10:21:59 +0100

    Add support for cherry-picking the YANG modules to install
    sysrepoctl can detect the dependencies of a YANG module (like other
    imported modules) and will install them too automatically. There is no
    need to install those modules separately.
    
    If the "${YANG_MODULES_DIR}/install" directory exists only the modules
    listed will be installed.

--- load-yang-modules.sh
+++ load-yang-modules.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for f in `ls ${YANG_MODULES_DIR}/*.yang`; do
+if [ -d ${YANG_MODULES_DIR}/install ]; then MODULES=$(ls ${YANG_MODULES_DIR}/install | sed -e "s#^#${YANG_MODULES_DIR}/#");
+else MODULES=$(ls ${YANG_MODULES_DIR}/*.yang); fi
+for f in ${MODULES}; do
 	if head -n1 $f | grep ^submodule > /dev/null 2>&1; then echo "Skipping submodule $f"; continue; fi
 	echo "Loading module $f"
 	time sysrepoctl --search-dirs ${YANG_MODULES_DIR} --install $f -v3
