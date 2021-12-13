d990d4abe4d6a9988f7b706ccb57c6d931b5cf1a
Marko Zagožen <marko.zagozen@gmail.com>
2021-12-13 15:57:27 +0100

    Stop installing YANG modules on error

--- install-yang-modules.sh
+++ install-yang-modules.sh
@@ -2,8 +2,11 @@
 
 if [ -d ${YANG_MODULES_DIR}/install ]; then MODULES=$(ls ${YANG_MODULES_DIR}/install | sed -e "s#^#${YANG_MODULES_DIR}/#");
 else MODULES=$(ls ${YANG_MODULES_DIR}/*.yang); fi
+
+# Stop installing modules on errors
+set -e
 for f in ${MODULES}; do
 	if head -n1 $f | grep ^submodule > /dev/null 2>&1; then echo "Skipping submodule $f"; continue; fi
-	echo "Loading module $f"
+	echo "Installing module $f"
 	time sysrepoctl --search-dirs ${YANG_MODULES_DIR} --install $f -v3
 done
