5af9b1f956ea4425a0abd62ed80c4f8a5c142235
Marko Zagožen <marko.zagozen@gmail.com>
2022-08-30 16:19:53 +0200

    Print progress from install-yang-modules.sh

--- install-yang-modules.sh
+++ install-yang-modules.sh
@@ -3,10 +3,13 @@
 if [ -d ${YANG_MODULES_DIR}/install ]; then MODULES=$(ls ${YANG_MODULES_DIR}/install | sed -e "s#^#${YANG_MODULES_DIR}/#");
 else MODULES=$(ls ${YANG_MODULES_DIR}/*.yang); fi
 
+n=0
+total=$(echo "${MODULES}" | wc -l)
 # Stop installing modules on errors
 set -e
 for f in ${MODULES}; do
-	if head -n1 $f | grep ^submodule > /dev/null 2>&1; then echo "Skipping submodule $f"; continue; fi
-	echo "Installing module $f"
+	n=$((n+1))
+	if head -n1 $f | grep ^submodule > /dev/null 2>&1; then echo "[$n/$total] Skipping submodule $f"; continue; fi
+	echo "[$n/$total] Installing module $f"
 	time sysrepoctl --search-dirs ${YANG_MODULES_DIR} --install $f -v3
 done
