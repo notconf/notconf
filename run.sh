79a9968617ba60968bb858ac8e2881bdf14da576
Marko Zagožen <marko.zagozen@gmail.com>
2023-04-03 12:22:26 +0200

    Tell netopeer to listen on local socket too

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 /install-yang-modules.sh
 /load-startup-config.sh
 mkdir -p /log
-exec netopeer2-server -d -v2 2>&1 | tee /log/netopeer.log
+exec netopeer2-server -U -d -v2 2>&1 | tee /log/netopeer.log
