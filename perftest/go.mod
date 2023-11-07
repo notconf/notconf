6e2e1cc9b53606b5f0599f50c29c36e06a048d4b
Marko Zagožen <marko.zagozen@gmail.com>
2023-11-07 21:44:32 +0100

    Use go NETCONF client for perf test
    ncclient (Python) has significant overhead of around 100ms per NETCONF
    RPC!?

---
+++ perftest/go.mod
@@ -0,0 +1,10 @@
+module perftest
+
+go 1.21.3
+
+require (
+	github.com/nemith/netconf v0.0.1
+	golang.org/x/crypto v0.14.0
+)
+
+require golang.org/x/sys v0.13.0 // indirect
---
+++ perftest/test-netconf.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"context"
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	"github.com/nemith/netconf"
+	ncssh "github.com/nemith/netconf/transport/ssh"
+	"golang.org/x/crypto/ssh"
+)
+
+var (
+	host       = flag.String("host", "localhost", "Hostname")
+	port       = flag.Int("port", 830, "Port")
+	username   = flag.String("username", "admin", "Username")
+	password   = flag.String("password", "admin", "Password")
+	n          = flag.Int("n", 10, "Number of times to repeat the request")
+	verbose    = flag.Bool("v", false, "Verbose")
+	getData    = flag.Bool("get-data", false, "Use <get-data> instead of <get-config>")
+	datastore  = flag.String("datastore", "running", "Datastore")
+	filter     = flag.String("filter", "", "Filter")
+	rawRpcFile = flag.String("raw-rpc", "", "Raw RPC file")
+)
+
+// OnlineVariance computes the sample variance incrementally using the Welford's algorithm
+type OnlineVariance struct {
+	ddof, n  int
+	mean, M2 float64
+}
+
+func NewOnlineVariance(ddof int) *OnlineVariance {
+	return &OnlineVariance{ddof: ddof}
+}
+
+func (ov *OnlineVariance) Include(datum float64) {
+	ov.n++
+	delta := datum - ov.mean
+	ov.mean += delta / float64(ov.n)
+	ov.M2 += delta * (datum - ov.mean)
+}
+
+func (ov *OnlineVariance) Variance() float64 {
+	return ov.M2 / float64(ov.n-ov.ddof)
+}
+
+func (ov *OnlineVariance) Std() float64 {
+	return math.Sqrt(ov.Variance())
+}
+
+func BuildConfig(username string, password string) *ssh.ClientConfig {
+	config := &ssh.ClientConfig{
+		User: username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	return config
+}
+
+type GetDataReply struct {
+	XMLName xml.Name `xml:"data"`
+	Reply   string   `xml:",innerxml"`
+}
+
+// GetData is a helper function to send a <get-data> request to the server
+func GetData(session *netconf.Session, datastore string, filter string) (*string, error) {
+	request := "<get-data xmlns='urn:ietf:params:xml:ns:yang:ietf-netconf-nmda'>"
+	if datastore != "" {
+		request += fmt.Sprintf("<datastore xmlns:ds='urn:ietf:params:xml:ns:yang:ietf-datastores'>ds:%s</datastore>", datastore)
+	}
+	if filter != "" {
+		request += fmt.Sprintf("<xpath-filter>%s</xpath-filter>", filter)
+	}
+	request += "</get-data>"
+	var reply GetDataReply
+	if err := session.Call(context.Background(), request, &reply); err != nil {
+		return nil, err
+	}
+	return &reply.Reply, nil
+}
+
+func main() {
+	flag.Parse()
+
+	if !*getData && *filter != "" {
+		log.Fatalf("filter can only be used with <get-data>")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	sshAddr := fmt.Sprintf("%s:%d", *host, *port)
+	transport, err := ncssh.Dial(ctx, "tcp", *&sshAddr, BuildConfig(*username, *password))
+	if err != nil {
+		panic(err)
+	}
+	defer transport.Close()
+
+	session, err := netconf.Open(transport)
+	if err != nil {
+		panic(err)
+	}
+
+	// timeout for the call itself.
+	ctx, cancel = context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	var getter func() (*string, error)
+
+	if *rawRpcFile != "" {
+		rawRpc, err := os.ReadFile(*rawRpcFile)
+		if err != nil {
+			log.Fatalf("failed to read raw-rpc file: %v", err)
+		}
+		getter = func() (*string, error) {
+			var reply GetDataReply
+			if err := session.Call(ctx, rawRpc, &reply); err != nil {
+				return nil, err
+			}
+			return &reply.Reply, nil
+		}
+	} else if *getData {
+		getter = func() (*string, error) {
+			return GetData(session, *datastore, *filter)
+		}
+	} else {
+		getter = func() (*string, error) {
+			deviceConfig, err := session.GetConfig(ctx, "running")
+			if err != nil {
+				return nil, err
+			}
+			dc := string(deviceConfig)
+			return &dc, nil
+		}
+	}
+
+	var ov = NewOnlineVariance(1)
+
+	// Repeat the request a few times
+	for i := 0; i < *n; i++ {
+		// Store the start time in nanoseconds
+		start := time.Now().UnixNano()
+
+		response, err := getter()
+		if err != nil {
+			log.Fatalf("failed to get data: %v", err)
+		}
+
+		end := time.Now().UnixNano()
+		ov.Include(float64(end - start))
+
+		if *verbose {
+			log.Printf("response:\n%s\n", *response)
+		}
+	}
+
+	if err := session.Close(context.Background()); err != nil {
+		log.Print(err)
+	}
+
+	var unit = "µ"
+	var scale = 1000.0
+	if ov.mean >= 1000 {
+		unit = "m"
+		scale = 1000000.0
+	}
+	fmt.Printf("--- netopeer %d: mean=%.6f %ss σ=%.6f %ss\n", *n, ov.mean/scale, unit, ov.Std()/scale, unit)
+}
