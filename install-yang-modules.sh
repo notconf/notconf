2bf1c640c60e219811fb3e8653a753b28c94fea0
Marko Zagožen <marko.zagozen@gmail.com>
2024-02-21 16:09:45 +0100

    Add option to enable module features
    Selectively enable YANG module features (if-feature statement) by
    providing the list of features in `enable-features.csv` file.
    
    Co-authored-by: Marco Polano <marco.polano@telecomitalia.it>
    Co-authored-by: Marko Zagožen <marko.zagozen@gmail.com>

--- install-yang-modules.sh
+++ install-yang-modules.sh
@@ -13,7 +13,10 @@ set -e
 # Skip submodules as they will automatically be installed with their parent
 for f in ${MODULES}; do
 	n=$((n+1))
-	if head -n1 $f | grep ^submodule > /dev/null 2>&1; then echo "[$n/$total] Skipping submodule $f"; continue; fi
+	if grep -m1 -P "^\s*submodule" $f > /dev/null 2>&1; then
+		echo "[$n/$total] Skipping submodule $f";
+		continue;
+	fi
 	echo "[$n/$total] Preparing to install module $f"
 	install_modules+=($f)
 done
@@ -29,3 +32,27 @@ install_modules=( "${install_modules[@]/#/--install }" )
 # Install all modules in a single batch (recent-ish feature
 # https://github.com/CESNET/netopeer2/issues/1337#issuecomment-1403225980)
 time sysrepoctl --search-dirs ${YANG_MODULES_DIR} ${install_modules[@]} -v3
+
+# Enable features
+# Read csv file with feature list
+if [ -f "${YANG_MODULES_DIR}/enable-features.csv" ]; then
+	prev_module=""
+	feature_enable=""
+	echo "Reading features to enable from file ${YANG_MODULES_DIR}/enable-features.csv"
+
+	while IFS=, read -r module_name feature_name || [ -n "$module_name$feature_name" ]; do
+		if [ "$prev_module" != "$module_name" ]; then
+			if [ ! -z "$prev_module" ]; then
+				sysrepoctl --change $prev_module $feature_enable -v3
+			fi
+			prev_module=$module_name
+			feature_enable=""
+		fi
+		feature_enable="$feature_enable --enable-feature $feature_name"
+	done < ${YANG_MODULES_DIR}/enable-features.csv
+	if [ ! -z "$prev_module" ]; then
+		sysrepoctl --change $prev_module $feature_enable -v3
+	fi
+else
+	echo 'Feature file "enable-features.csv" not present'
+fi
