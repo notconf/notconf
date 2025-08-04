41f0c0ffc020a44d0f9e5a1796cc05530af573bd
Marko Zagožen <marko.zagozen@gmail.com>
2025-08-04 13:15:19 +0200

    Fix typo

--- load-oper-data.py
+++ load-oper-data.py
@@ -51,7 +51,7 @@ def main():
     logging.info('done merging operational data')
     if args.sync_file:
         with open(args.sync_file, 'w') as f:
-            f.write(str(datetime.datetime.now(datatime.UTC)))
+            f.write(str(datetime.datetime.now(datetime.UTC)))
     signal.sigwait({signal.SIGINT, signal.SIGTERM})
     stop.set()
     t.join()
